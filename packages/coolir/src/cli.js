d2f34ea942a7a6308c0d37223a49fe130c2c8e33
Dan Lynch <pyramation@gmail.com>
2020-09-30 20:13:49 -0700

    updates

--- packages/coolir/src/cli.js
+++ packages/coolir/src/cli.js
@@ -7,12 +7,18 @@ import { clone as npmModule } from './npm-module';
 import { clone as styledWorkspace } from './styled-workspace';
 import { clone as styledWorkspaceModule } from './styled-workspace-module';
 import { clone as styledNpmModule } from './styled-npm-module';
+import { clone as launchQLServer } from './launchql-server';
+import { clone as graphileServer } from './graphile-server';
+import { clone as graphilePlugin } from './graphile-plugin';
 
 export const go = async () => {
   await cli({
     npmModule,
     workspace,
     workspaceModule,
+    launchQLServer,
+    graphileServer,
+    graphileServer,
     styledWorkspace,
     styledWorkspaceModule,
     styledNpmModule
